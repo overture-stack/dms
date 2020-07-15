459507582d90d98e721c1bf42a27308f32567903
rtisma <942951+rtisma@users.noreply.github.com>
2020-07-15 13:42:25 -0400

    Initial commit

---
+++ src/main/java/bio/overture/dms/DmsApplication.java
@@ -0,0 +1,12 @@
+package bio.overture.dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DmsApplication.class, args);
+  }
+}
