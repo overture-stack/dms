bbf335deef8770fd77669b6cb22c5ea729313ee6
rtisma <942951+rtisma@users.noreply.github.com>
2020-09-24 12:54:32 -0400

    cleanedup

--- install-via-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dms_exe=/usr/bin/dms
-echo "#!/bin/bash" > $dms_exe
-#echo 'docker run --rm -it -u $(id -u):$(id -g) rob dms $@' >> $dms_exe
-echo 'docker run --rm -it overture/dms:latest dms $@' >> $dms_exe
-chmod +x $dms_exe
