9d4f80e061ba33cf6dc7d7b655104d9e43d6765a
rtisma <942951+rtisma@users.noreply.github.com>
2020-12-01 18:27:52 -0500

    updated textio properties

--- src/main/java/bio/overture/dms/cli/command/spec/SpecConfigCommand.java
+++ src/main/java/bio/overture/dms/cli/command/spec/SpecConfigCommand.java
@@ -13,7 +13,6 @@ import bio.overture.dms.model.spec.EgoSpec.SSOSpec;
 import bio.overture.dms.util.ObjectSerializer;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Optional;
 import java.util.concurrent.Callable;
 import java.util.function.BiConsumer;
 import lombok.NonNull;
