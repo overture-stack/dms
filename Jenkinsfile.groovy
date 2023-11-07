facc9b498e1fe9a793d1d468bb82149d2a2189ba
Anders Richardsson <2107110+caravinci@users.noreply.github.com>
2023-11-07 16:09:26 -0500

    produce testing images

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -93,137 +93,35 @@ spec:
         }
         stage('Build & Publish Develop') {
             when {
-                branch "develop"
+                branch 'jbrowseintegeration'
             }
             steps {
                 container('docker') {
                     withCredentials([usernamePassword(credentialsId:'OvertureDockerHub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
                         sh 'docker login -u $USERNAME -p $PASSWORD'
                     }
-                    sh "docker build --network=host --target client -f  Dockerfile . -t overture/dms:edge -t overture/dms:${version}-${commit}"
-                    sh "docker build --network=host --target latest-version-helper -f Dockerfile . -t overture/dms-version-helper:edge"
-                    sh "docker push overture/dms:${version}-${commit}"
-                    sh "docker push overture/dms:edge"
-                    sh "docker push overture/dms-version-helper:edge"
+                    sh "docker build --network=host --target client -f  Dockerfile . -t overture/dms:jbrowse-${commit}"
+                    sh "docker push overture/dms:jbrowse-${commit}"
+
+                    sh "docker build --network=host --target latest-version-helper -f Dockerfile . -t overture/dms-version-helper:jbrowse-${commit}"
+                    sh "docker push overture/dms-version-helper:jbrowse-${commit}"
                 }
                 container('docker') {
                     withCredentials([usernamePassword(credentialsId:'OvertureBioGithub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
                         sh 'docker login ghcr.io -u $USERNAME -p $PASSWORD'
                     }
-                    sh "docker build --network=host --target client -f  Dockerfile . -t ${dockerOrg}/${dmsRepo}:edge -t ${dockerOrg}/${dmsRepo}:${version}-${commit}"
-                    sh "docker build --network=host --target latest-version-helper -f Dockerfile . -t ${dockerOrg}/${dmsVersionHelperRepo}:edge"
-                    sh "docker build --target insecure --network=host -f ./nginx/path-based/Dockerfile ./nginx/path-based -t ${dockerOrg}/${dmsGatewayRepo}:edge"
+                    sh "docker tag jbrowse-${commit} ${dockerOrg}/${dmsRepo}:jbrowse-${commit}"
+                    sh "docker push ${dockerOrg}/${dmsRepo}:jbrowse-${commit}"
+
+                    sh "docker tag overture/dms-version-helper:jbrowse-${commit} ${dockerOrg}/${dmsVersionHelperRepo}:jbrowse-${commit}"
+                    sh "docker push ${dockerOrg}/${dmsGatewayRepo}:jbrowse-${commit}"
+
+                    sh "docker build --target insecure --network=host -f ./nginx/path-based/Dockerfile ./nginx/path-based -t ${dockerOrg}/${dmsGatewayRepo}:jbrowse-${commit}"
+                    sh "docker push ${dockerOrg}/${dmsGatewayRepo}-secure:jbrowse-${commit}"
+
                     sh "docker build --target secure --network=host -f ./nginx/path-based/Dockerfile ./nginx/path-based -t ${dockerOrg}/${dmsGatewayRepo}-secure:edge"
-                    sh "docker push ${dockerOrg}/${dmsRepo}:${version}-${commit}"
-                    sh "docker push ${dockerOrg}/${dmsRepo}:edge"
-                    sh "docker push ${dockerOrg}/${dmsGatewayRepo}:edge"
-                    sh "docker push ${dockerOrg}/${dmsVersionHelperRepo}:edge"
+                    sh "docker push ${dockerOrg}/${dmsVersionHelperRepo}:jbrowse-${commit}"
                 }
-            }
-        }
-        stage('Release & tag') {
-          when {
-            branch "master"
-          }
-          steps {
-                container('docker') {
-                    withCredentials([usernamePassword(credentialsId: 'OvertureBioGithub', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-                        sh "git tag ${version}"
-                        sh "git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/overture-stack/dms --tags"
-                    }
-                    withCredentials([usernamePassword(credentialsId:'OvertureBioGithub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-                        sh 'docker login ghcr.io -u $USERNAME -p $PASSWORD'
-                    }
-                    sh "docker build --network=host --target client -f Dockerfile . -t ${dockerOrg}/${dmsRepo}:latest -t ${dockerOrg}/${dmsRepo}:${version}"
-                    sh "docker build --network=host --target latest-version-helper  -f Dockerfile . -t ${dockerOrg}/${dmsVersionHelperRepo}:latest"
-                    sh "docker build --target insecure --network=host -f ./nginx/path-based/Dockerfile ./nginx/path-based -t ${dockerOrg}/${dmsGatewayRepo}:latest -t ${dockerOrg}/${dmsGatewayRepo}:${version}"
-                    sh "docker build --target secure --network=host -f ./nginx/path-based/Dockerfile ./nginx/path-based -t ${dockerOrg}/${dmsGatewayRepo}-secure:latest -t ${dockerOrg}/${dmsGatewayRepo}-secure:${version}"
-                    sh "docker push ${dockerOrg}/${dmsRepo}:${version}"
-                    sh "docker push ${dockerOrg}/${dmsGatewayRepo}:${version}"
-                    sh "docker push ${dockerOrg}/${dmsGatewayRepo}-secure:${version}"
-                    sh "docker push ${dockerOrg}/${dmsGatewayRepo}:latest"
-                    sh "docker push ${dockerOrg}/${dmsGatewayRepo}-secure:latest"
-                    sh "docker push ${dockerOrg}/${dmsRepo}:latest"
-                    sh "docker push ${dockerOrg}/${dmsVersionHelperRepo}:latest"
-                }
-                container('docker') {
-                  withCredentials([usernamePassword(credentialsId:'OvertureDockerHub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-                      sh 'docker login -u $USERNAME -p $PASSWORD'
-                  }
-                  sh "docker build --network=host --target client -f Dockerfile . -t overture/dms:latest -t overture/dms:${version}"
-                  sh "docker build --network=host --target latest-version-helper  -f Dockerfile . -t overture/dms-version-helper:latest"
-                  sh "docker push overture/dms:${version}"
-                  sh "docker push overture/dms:latest"
-                  sh "docker push overture/dms-version-helper:latest"
-                }
-            }
-        }
-
-        stage('Destination SNAPSHOT') {
-            when {
-                anyOf {
-                    branch 'develop'
-                    branch 'test-develop'
-                }
-            }
-            steps {
-                script {
-                    repo = "dcc-snapshot/bio/overture"
-                }
-            }
-        }
-
-        stage('Destination release') {
-            when {
-                anyOf {
-                    branch 'master'
-                    branch 'test-master'
-                }
-            }
-            steps {
-                script {
-                    repo = "dcc-release/bio/overture"
-                }
-            }
-        }
-
-        stage('Upload Artifacts') {
-            when {
-                anyOf {
-                    branch 'master'
-                    branch 'test-master'
-                    branch 'develop'
-                    branch 'test-develop'
-                }
-            }
-            steps {
-                script {
-                    
-                    project = "dms"
-                    versionName = "$version"
-                    subProjects = ['cli']
-
-                    files = []
-                    files.add([pattern: "pom.xml", target: "$repo/$project/$versionName/$project-${versionName}.pom"])
-
-                    for (s in subProjects) {
-                        name = "${project}-$s"
-                        target = "$repo/$name/$versionName/$name-$versionName"
-                        files.add(pom(name, target))
-                        files.add(jar(name, target))
-
-                        if (s in ['cli']) {
-                            files.add(runjar(name, target))
-                            files.add(tar(name, target))
-                        }
-                    }
-
-                    fileSet = JsonOutput.toJson([files: files])
-                    pretty = JsonOutput.prettyPrint(fileSet)
-                    print("Uploading files=${pretty}")
-                }
-
-                rtUpload(serverId: 'artifactory', spec: fileSet)
             }
         }
     }
